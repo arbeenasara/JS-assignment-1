5e1520d1026dece7561ada87bb2efa7ee1575ddd
arbeenasara <arbeena27memon@gmail.com>
2020-06-14 18:40:38 +0500

    Assignment

---
+++ app.js
@@ -0,0 +1,201 @@
+document.getElementById("demo").innerHTML = "<h1>Chapter 1</h1>";
+
+// document.write("<h3>Question # 1</h3>");
+// window.alert("Hello! Welcome to my website...");
+
+
+// document.write("<h3>Question # 2</h3>");
+// window.alert("Error! Please enter a valid password.");
+
+
+// document.write("<h3>Question # 3</h3>");
+// alert("Welcome to JS land...\nHappy Coding!");
+
+
+// document.write("<h3>Question # 4</h3>");
+// window.alert("Welcome to JS land...");
+// window.alert("Happy Coding!");
+
+
+// document.write("<h3>Question # 5</h3>");
+// var a = "Hello.. I can run JS through my web browser's console"
+// console.log(a);
+// alert(a);
+
+document.getElementById("comment").innerHTML = "answers are in comment";
+
+document.getElementById("ch2").innerHTML = "<h1>Chapter 2</h1>";
+document.getElementById("heading").innerHTML = "<h3>Question # 1</h3>";
+var userName = "Arbeena";
+document.getElementById("username").innerHTML = userName;
+
+
+document.getElementById("heading2").innerHTML = "<h3>Question # 2</h3>";
+var x = "Arbeena ";
+var y = "Altaf";
+var myName = x + y;
+document.getElementById("myname").innerHTML = myName;
+
+
+document.getElementById("heading3").innerHTML = "<h3>Question # 3 </h3> answers are in comment";
+// var message = "Hello World";
+// alert(message);
+
+
+document.getElementById("heading4").innerHTML = "<h3>Question # 4</h3> answers are in comment";
+// var a = "John Doe";
+// var b = "15 years old";
+// var c = "Certified Mobile Application Development";
+// alert(a);
+// alert(b);
+// alert(c);
+
+
+document.getElementById("heading5").innerHTML = "<h3>Question # 5</h3> answers are in comment";
+// var i = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// window.alert(i);
+
+
+document.getElementById("heading6").innerHTML = "<h3>Question # 6</h3> answers are in comment";
+// var email = "arbeena27memon@gmail.com";
+// alert("My email address is " + email);
+
+
+document.getElementById("heading7").innerHTML = "<h3>Question # 7</h3> answers are in comment";
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+
+document.getElementById("heading8").innerHTML = "<h3>Question # 8</h3> answers are in comment";
+// document.write("Yah! I can write HTML content through JavaScript");
+
+
+document.getElementById("heading9").innerHTML = "<h3>Question # 9</h3> answers are in comment";
+// document.write("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬");
+// var i = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
+// alert(i);
+
+
+document.getElementById("ch3").innerHTML = "<h1>Chapter 3</h1>";
+document.getElementById("qu1").innerHTML = "<h3>Question # 1</h3> answers are in comment";
+// var age = 21;
+// alert("I am " + age + " years old");
+
+
+document.getElementById("qu2").innerHTML = "<h3>Question # 2</h3> answers are in comment";
+// var num = 4;
+// alert("You have visited this site " + num + " times");
+
+
+document.getElementById("qu3").innerHTML = "<h3>Question # 3</h3>";
+var birthYear = 1998;
+document.getElementById("year").innerHTML = "My birth year is " + birthYear + "<br> Data type of my declared variable is number";
+
+
+document.getElementById("qu4").innerHTML = "<h3>Question # 4</h3>";
+var visitorName = "John Doe";
+var productTitle = " T-shirt";
+var quantity = 5;
+var store = quantity + productTitle;
+document.getElementById("store").innerHTML = visitorName + " ordered " + store + "(s) on XYZ Clothing store";
+
+
+document.getElementById("ch4").innerHTML = "<h1>Chapter 4</h1>";
+document.getElementById("que1").innerHTML = "<h3>Question # 1</h3>";
+var i = 42;
+var a = 23;
+var b = 5;
+var total = i + a + b;
+document.getElementById("variables3").innerHTML = total;
+
+
+document.getElementById("que2").innerHTML = "<h3>Question # 2</h3>";
+document.getElementById("legal").innerHTML = "<h4>Legal names</h4>var test <br> var test1 <br> var bioData <br> var user_2 <br> var $user";
+document.getElementById("illegal").innerHTML = "<h4>Illegal names</h4>var 1user <br> var bio data <br> var alert <br> var user2_ <br> var .user123";
+
+
+document.getElementById("que3").innerHTML = "<h3>Question # 3</h3>";
+document.getElementById("exercise").innerHTML = "<h4>Rules for naming JS variables</h4> Variable names can only contain numbers, $ and _. For example: $my_1stVariable <br> Variables must begin with a letter, $ or _. For example: $name, _name or name <br> Variable names are case sensitive <br> Variable names should not be JS keywords";
+
+
+document.getElementById("ch5").innerHTML = "<h1>Chapter 5</h1>";
+document.getElementById("q1").innerHTML = "<h3>Question # 1</h3>";
+var a = 3;
+var b = 5;
+var i = a + b;
+document.getElementById("sum").innerHTML = "Sum of " + a + " and " + b + " is " + i;
+
+
+document.getElementById("q2").innerHTML = "<h3>Question # 2</h3>";
+var a = 3;
+var b = 5;
+var i = a - b;
+document.getElementById("subtract").innerHTML = "<h4>Subtract</h4>" + "Sum of " + a + " and " + b + " is " + i;
+
+var a = 3;
+var b = 5;
+var i = a * b;
+document.getElementById("multiplication").innerHTML = "<h4>Multiplication</h4>" + "Sum of " + a + " and " + b + " is " + i;
+
+var a = 3;
+var b = 5;
+var i = a / b;
+document.getElementById("division").innerHTML = "<h4>Division</h4>" + "Sum of " + a + " and " + b + " is " + i;
+
+var a = 3;
+var b = 5;
+var i = a % b;
+document.getElementById("modulus").innerHTML = "<h4>Modulus</h4>" + "Sum of " + a + " and " + b + " is " + i;
+
+
+document.getElementById("q3").innerHTML = "<h3>Question # 3</h3>";
+var a = undefined;
+document.getElementById("exb").innerHTML = "Value after variable declaration is " + a;
+
+var b = 5;
+document.getElementById("exc").innerHTML = "Initial value is: " + b;
+
+var c = 5;
+var i = c + 1;
+document.getElementById("exd").innerHTML = "Value after increment is: " + i;
+
+var d = 6;
+var i = d + 7;
+document.getElementById("exe").innerHTML = "Value after addition is: " + i;
+
+var i = 13;
+var a = i - 1;
+document.getElementById("exe").innerHTML = "Value after decreament is: " + a;
+
+var a = 5;
+var i = a / 3;
+document.getElementById("exf").innerHTML = "The remainder is: " + i;
+
+
+document.getElementById("q4").innerHTML = "<h3>Question # 4</h3>";
+var cost = 600;
+var i = cost * 5;
+document.getElementById("cost").innerHTML = "Total cost to buy 5 tickets to a movie is " + i + "PKR";
+
+
+document.getElementById("q5").innerHTML = "<h3>Question # 5</h3>";
+var num = 4;
+ 
+var num = parseInt(num); //parse the num to number
+var i = 0;
+document.write("<h4>Table of 4</h4><table>");
+
+for(i=1;i<11;i++) {
+    document.write("<tr><td>" + num +  "</td><td>x</td><td>" + i +  "</td><td>=</td><td>" + num*i + "</td></tr>");
+}
+document.write("</table>");
+
+
+
+
+
+
+
+
+
+
